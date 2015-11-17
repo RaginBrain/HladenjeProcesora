ef669716960f7d7bab815b1c6e867f9e6ef5d29e
RaginBrain <inverted2logic@gmail.com>
2015-11-18 00:25:02 +0100

    Napravljen Uvod

--- Carbon Nanotubes Aid Microprocessor Cooling_files/show-hide-content.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-  
-  if (getCookie('email-to-pardot') == '1') {
-    jQuery('.restricted-content').show();
-  } else {
-    jQuery('.restricted-form').show();
-  }
-
-});
--- Cooling Microprocessors with Carbon Nanotubes _ Berkeley Lab_files/global.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){ 
-
-    jQuery('h3.rpwe-title a').unwrap();
-    jQuery('ul.rpwe-ul li').removeClass('rpwe-clearfix clearfix cl');
-
-});
